16d499e89e76b36610525bce9e310fcb086c791b
UfukAtalay39 <atalayu@mef.edu.tr>
2025-10-06 17:52:07 +0300

    Java ile oluşturuldu

---
+++ BackEnd/SmartTravel/src/main/java/com/example/SmartTravel/SmartTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelApplication.class, args);
+	}
+
+}
---
+++ BackEnd/SmartTravel/src/test/java/com/example/SmartTravel/SmartTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
